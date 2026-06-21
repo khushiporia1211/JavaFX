24a1d7861e865cfd9340c540ad49b04f3fa749bd
khushi <khushiporia09@gmail.com>
2026-06-21 11:51:03 +0530

    initial commit

--- src/propertydemo/PropertyDemo.java
+++ src/propertydemo/PropertyDemo.java
@@ -1,4 +1,37 @@
 package propertydemo;
 
-public class PropertyDemo {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class PropertyDemo extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Button b = new Button("_Click Me");
+        b.setTextFill(Color.BLUE);
+        b.setMnemonicParsing(true);
+        Tooltip tp = new Tooltip("Click this button to save Data");
+        b.setTooltip(tp);
+//        b.setStyle("-fx-border-color:yellow;-fx-background-color:red;");
+        Alert a = new Alert(Alert.AlertType.INFORMATION,"Button is Clicked");
+
+        b.setOnAction(e->a.show());
+
+        FlowPane fp = new FlowPane(b);
+        Scene sc = new Scene(fp,400,400);
+        stage.setScene(sc);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
 }
